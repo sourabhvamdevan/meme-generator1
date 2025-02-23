77989e62c2bde2db223bca75f07e8c807dac8c92
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2025-02-23 15:01:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/meme-gen-3/',
+  base: '/meme-generator1/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
